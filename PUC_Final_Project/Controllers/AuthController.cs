c8eb77023c08bb1e007f48b6378190ae04a95810
Henrique Vieira <henrique.v.santos@outlook.com>
2022-07-27 00:06:56 -0300

    Adição de options no Program.cs para que o JWT funcionasse

--- PUC_Final_Project/Controllers/AuthController.cs
+++ PUC_Final_Project/Controllers/AuthController.cs
@@ -9,6 +9,7 @@ namespace PUC_Final_Project.Controllers
 {
     public class AuthController : Controller
     {
+
         [HttpPost, Route("login")]
         public IActionResult Login(Login model)
         {
@@ -16,13 +17,13 @@ namespace PUC_Final_Project.Controllers
             {
                 return BadRequest("Invalid client request");
             }
-            if (model.UserName == "johndoe" && model.Password == "johndoe2410")
+            if (model.UserName == "henrique" && model.Password == "henrique123")
             {
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@2410"));
                 var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
                 var tokenOptions = new JwtSecurityToken(
                     issuer: "CodeMaze",
-                    audience: "https://localhost:5001",
+                    audience: "https://localhost:7245",
                     claims: new List<Claim>(),
                     expires: DateTime.Now.AddMinutes(5),
                     signingCredentials: signinCredentials
--- PUC_Final_Project/Program.cs
+++ PUC_Final_Project/Program.cs
@@ -1,20 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using PUC_Final_Project.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<BrasileiraoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BrasileiraoContext")));
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.Authority = "http://localhost:7245";
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                ValidateIssuer = true,
+                ValidIssuer = "Also My Issuer",    //Missing line here
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                ValidateAudience = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("abcdefghijklmnopqrstuvwxyz123456"))
+            };
+        });
+
 builder.Services.AddSwaggerGen(opt =>
 {
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "PUC Final Project", Version = "v1" });
     opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
@@ -22,7 +43,8 @@ builder.Services.AddSwaggerGen(opt =>
         Name = "Authorization",
         Type = SecuritySchemeType.Http,
         BearerFormat = "JWT",
-        Scheme = "bearer"
+        Scheme = "Bearer"
+
     });
     opt.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
@@ -49,8 +71,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
 app.Run();
+
+
+
+
+
